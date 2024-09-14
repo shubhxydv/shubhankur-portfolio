265f0d79561c2019d148e2ab2aae7cd8ef8c7d7a
Shubhankur <sy7564@srmist.edu.in>
2024-09-14 14:10:28 +0530

    last commit

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,52 +1,3 @@
-// import React from "react";
-// import Technova from '../assets/images/Technova.jpg';
-// import isro from '../assets/images/isro.jpg';
-// import AICTE from '../assets/images/AICTE.jpg';
-// import data_science from '../assets/images/data_science.jpg';
-
-// import '../assets/styles/Project.scss';
-
-// function Project() {
-//     return(
-//     <div className="projects-container" id="projects">
-//         <h1> Achievments</h1>
-//         <div className="projects-grid">
-//             <div className="project">
-//                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={AICTE} className="zoom" alt="thumbnail" width="100%"/></a>
-//                <h2>AICTE Android Developer Virtual Internship </h2>
-//                 <p>Completed a transformative 10-week Android Developer Virtual Internship, supported by Google for Developers (April-June 2024), 
-//                     through EduSkills Foundation and AICTE.</p>
-//             </div>
-//             <div className="project">
-//                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={isro} className="zoom" alt="thumbnail" width="100%"/></a>
-//                 <h2>ISRO Course on AIML Geospatial Data Analysis </h2>
-//                 <p> 
-//                 Successfully completed an online course on AI and Machine Learning for Geospatial Data Analysis, 
-//                 organized by IIRS-ISRO, Dehradun.</p>
-//             </div>
-//             <div className="project">
-//                 <a href="" target="_blank" rel="noreferrer"><img src={Technova} className="zoom" alt="thumbnail" width="100%"/></a>
-//                 <h2>Tech Nova Hackathon</h2>
-//                 <p>Participated in the TechNova: Igniting Brilliance (Season 1) hackathon, organized by Enterprise System Solution Private Limited, and recognized for hard work and dedication. Selected for Round 2 Grand Masters (ongoing).</p>
-//             </div>
-//             <div className="project">
-//                 <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={data_science} className="zoom" alt="thumbnail" width="100%"/></a>
-//            <h2>Altair Data Science Certification</h2>
-//                 <p>  
-//                 I have successfully passed the Data Engineering Professional Certification examination, 
-//                 achieving the Professional level. This certification reflects solid experience in data
-//                  access, basic transformations, working with multiple data sets, pivot tables, routines, 
-//                  and simple text processing.</p>
-//             </div>
-            
-//         </div>
-//     </div>
-//     );
-// }
-
-// export default Project;
-
-
 
 import React from "react";
 import Technova from '../assets/images/Technova.jpg';
@@ -65,7 +16,7 @@ interface ProjectDetails {
 
 const projects: ProjectDetails[] = [
     {
-        href: "https://www.filmate.club/",
+        href: "",
         imgSrc: AICTE,
         imgAlt: "AICTE Android Developer Virtual Internship thumbnail",
         title: "AICTE Android Developer Virtual Internship",
@@ -73,7 +24,7 @@ const projects: ProjectDetails[] = [
                       supported by Google for Developers (April-June 2024), through EduSkills Foundation and AICTE.`,
     },
     {
-        href: "https://yujisatojr.itch.io/highspeedchase",
+        href: "",
         imgSrc: isro,
         imgAlt: "ISRO AIML Geospatial Data Analysis thumbnail",
         title: "ISRO Course on AIML Geospatial Data Analysis",
@@ -81,7 +32,7 @@ const projects: ProjectDetails[] = [
                       organized by IIRS-ISRO, Dehradun.`,
     },
     {
-        href: "", // Empty href corrected with no link
+        href: "",
         imgSrc: Technova,
         imgAlt: "Tech Nova Hackathon thumbnail",
         title: "Tech Nova Hackathon",
@@ -90,7 +41,7 @@ const projects: ProjectDetails[] = [
                       Selected for Round 2 Grand Masters (ongoing).`,
     },
     {
-        href: "https://www.datumlearn.com/",
+        href: "",
         imgSrc: data_science,
         imgAlt: "Altair Data Science Certification thumbnail",
         title: "Altair Data Science Certification",
